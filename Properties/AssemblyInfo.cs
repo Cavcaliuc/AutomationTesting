dd18201aab34c13eb941555946f17c6d20547e06
Cavcaliuc <cavcaliucnina09@gmail.com>
2020-11-17 20:25:47 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc4cf749-1516-4248-ba5a-1e5aa6641dca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTesting 
+
+{
+    [TestClass]
+    public class MS_Test_Cross_Browser_Test
+    {
+        String test_url = "https://www.ebay.com/";
+
+        [TestMethod]
+        public void NavigateToDoApp()
+        {
+            IWebDriver driver;
+
+            // Local Selenium WebDriver
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(test_url);
+
+            driver.Manage().Window.Maximize();
+
+            // Click on Search box
+            IWebElement searchBox = driver.FindElement(By.CssSelector("div[id='gh-ac-box2'] input"));
+            searchBox.Click();
+            // Write something
+            searchBox.SendKeys("Computer");
+            searchBox.SendKeys(Keys.Enter);
+
+            IWebElement header = driver.FindElement(By.CssSelector("header[id='gh']"));
+
+            Console.WriteLine("\n-------------------------\n");
+            Console.WriteLine("TEST PASSED");
+            Console.WriteLine("\n-------------------------\n");
+
+            driver.Quit();
+        }
+    }
+}
+
